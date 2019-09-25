cefce513accec77f24a82820925e4d9f375f2f50
Rudy Garcia Alvarado <c01010010@hotmail.com>
2019-09-25 18:06:34 -0300

    feat: new model definition

--- src/exploration/vgg16.py
+++ src/exploration/vgg16.py
@@ -16,35 +16,40 @@ def get_vgg_encoder(input_height=224, input_width=224, pretrained='imagenet'):
     # Block1
     vgg16.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1'))
     vgg16.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2'))
-    vgg16.add(tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool'))
+    f1 = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')
+    vgg16.add(f1)
 
     # Block 2
     vgg16.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1'))
     vgg16.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2'))
-    vgg16.add(tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool'))
+    f2 = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')
+    vgg16.add(f2)
 
     # Block 3
     vgg16.add(tf.keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1'))
     vgg16.add(tf.keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2'))
     vgg16.add(tf.keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3'))
-    vgg16.add(tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool'))
+    f3 = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')
+    vgg16.add(f3)
 
     # Block 4
     vgg16.add(tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1'))
     vgg16.add(tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2'))
     vgg16.add(tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3'))
-    vgg16.add(tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool'))
+    f4 = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')
+    vgg16.add(f4)
 
     # Block 5
     vgg16.add(tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1'))
     vgg16.add(tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2'))
     vgg16.add(tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3'))
-    vgg16.add(tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool'))
+    f5 = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')
+    vgg16.add(f5)
 
     #if pretrained == 'imagenet':
     #    VGG_Weights_path = keras.utils.get_file(weights_url.split("/")[-1], weights_url)
     #    Model(img_input, x).load_weights(VGG_Weights_path)
 
-    return vgg16
+    return vgg16, [f1, f2, f3, f4, f5]
 
-vgg16 = get_vgg_encoder()
+vgg, layers = get_vgg_encoder()
--- src/exploration/vgg_unet.py
+++ src/exploration/vgg_unet.py
@@ -1,28 +1,58 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf
-from .vgg16 import get_vgg_encoder
+from src.exploration.vgg16 import get_vgg_encoder
 
 
 class VggUnet(tf.keras.Model):
 
-    def __init__(self, num_classes=10):
+    def __init__(self, input_height, input_width, num_classes=10):
         super(VggUnet, self).__init__(name='my_model')
         self.num_classes = num_classes
 
         # Getting the VGG Model
-        self.model = get_vgg_encoder()
+        self._build(input_height, input_width)
 
-    def call(self, inputs):
-        # Define your forward pass here,
-        # using layers you previously defined (in `__init__`).
-        x = self.dense_1(inputs)
-        return self.dense_2(x)
+    def _build(self, input_height, input_width):
 
-    def compute_output_shape(self, input_shape):
-        # You need to override this function if you want to use the subclassed model
-        # as part of a functional-style model.
-        # Otherwise, this method is optional.
-        shape = tf.TensorShape(input_shape).as_list()
-        shape[-1] = self.num_classes
-        return tf.TensorShape(shape)
+        l1_skip_conn = True
+        MERGE_AXIS = -1
+        # Builds the U-Net since the VGG architecture
+        model, levels = get_vgg_encoder(input_height=input_height, input_width=input_width)
+        [f1, f2, f3, f4, f5] = levels
+
+        # First u-net block
+        model.add(tf.keras.layers.ZeroPadding2D((1, 1)))
+        model.add(tf.keras.layers.Conv2D(512, (3, 3), padding='valid'))
+        model.add(tf.keras.layers.BatchNormalization())
+
+        # Second u-net block
+        to_concatenate = tf.keras.layers.UpSampling2D((2, 2))
+        model.add(to_concatenate)
+        model.add(tf.keras.layers.concatenate(input=[to_concatenate, f3]))
+        model.add(tf.keras.layers.ZeroPadding2D((1, 1)))
+        model.add(tf.keras.layers.Conv2D(256, (3, 3), padding='valid'))
+        model.add(tf.keras.layers.BatchNormalization())
+
+        # Third u-net layer
+        to_concatenate = tf.keras.layers.UpSampling2D((2, 2))
+        model.add(to_concatenate)
+        model.add(tf.keras.layers.concatenate([to_concatenate, f2], axis=MERGE_AXIS))
+        model.add(tf.keras.layers.ZeroPadding2D((1, 1)))
+        model.add(tf.keras.layers.Conv2D(128, (3, 3), padding='valid'))
+        model.add(tf.keras.layers.BatchNormalization())
+        to_concatenate = tf.keras.layers.UpSampling2D((2, 2))
+        model.add(to_concatenate)
+
+        if l1_skip_conn:
+            model.add(tf.keras.layers.concatenate([to_concatenate, f1], axis=MERGE_AXIS))
+
+        model.add(tf.keras.layers.ZeroPadding2D((1, 1)))
+        model.add(tf.keras.layers.Conv2D(64, (3, 3), padding='valid'))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.Conv2D(self.num_classes, (3, 3), padding='same'))
+
+        self.model = model
+
+model = VggUnet(224, 224)
+
