be33ac059c9260be0ead89303290b53333968897
Ignacio Zurita <52304845+iazt@users.noreply.github.com>
2019-09-26 03:15:31 -0300

    Update preprocessing.py

--- src/exploration/preprocessing.py
+++ src/exploration/preprocessing.py
@@ -8,20 +8,11 @@ from keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
 
 
-def preprocessing(X,y): 
+def preprocessing(X): 
     X_shape=X.shape
     X=X.reshape(X_shape[0],X_shape[1],X_shape[2],1)
     
     X=tf.image.resize_images(X,
                       size=[224,224])
     
-    datagen = ImageDataGenerator(
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    horizontal_flip=True,
-    vertical_flip= True)
-    
-
-    datagen.fit(X)
-    return datagen
+    return tf.concat([X,X,X],axis=3)
